783ff1d50945394d815f723cb3f1a00bdf9f38ea
abhijit-murthy <abhijit.murthy@gmail.com>
2013-07-11 01:03:49 -0500

    Added data models

---
+++ Parkifry/src/com/example/parkifry/ParkingSpot.java
@@ -0,0 +1,37 @@
+package com.example.parkifry;
+
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class ParkingSpot {
+	private double[] location;
+	private String name;
+	private Date creationDateTime;
+	
+	public ParkingSpot(double[] location,String name){
+		this.location = location;
+		this.name = name;
+		this.creationDateTime = Calendar.getInstance().getTime();
+	}
+	
+	public double[] getLocation(){
+		return location;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public Date getCreationDateTime(){
+		return creationDateTime;
+	}
+	
+	public void setLocation(double[] location){
+		this.location = location;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+}
